a9606abbdeff72313cf507e820bcc134b475f6ae
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-01 12:29:15 -0400

    If or Else.

---
+++ IfElseClass.java
@@ -0,0 +1,16 @@
+package com.aktar.switching;
+
+public class IfElseClass {
+
+	public static void main(String[] args) {
+	int Girls;
+	Girls=20;
+	if (Girls!=20.5){
+		System.out.println("They can go to Party");
+	}else{
+		System.out.println("They have to stay Home");
+	}
+
+	}
+
+}
