44e0b72f569881736a408e89f4fc9fca423d7706
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-01 20:46:18 -0400

    Begining Java Class

---
+++ BeginerClass1.java
@@ -0,0 +1,17 @@
+package com.java.beginers;
+import java.util.Scanner;
+
+public class BeginerClass1 {
+
+	public static void main(String[] args) {
+System.out.println("Hello This is my First Class");
+System.out.println("And I Feel very Interested about it");
+System.out.println("Its a different world");
+System.out.println("It is very amazing");
+System.out.print("And I like it");
+	}
+	
+	
+}
+
+
